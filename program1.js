35ac1aa0bcb3441c5a506eb34c6f0f2c5262bf8f
pratham gurjar <prathamggurjar@gmail.com>
2024-10-07 15:26:23 +0530

    done

--- program1.js
+++ program1.js
@@ -3,7 +3,29 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+    const matchingBrackets = {
+        ')': '(',
+        ']': '[',
+        '}': '{'
+    };
+    // Stack to keep track of opening brackets
+    const stack = [];
+
+    for (let char of s) {
+        if (matchingBrackets[char]) {
+            // If it's a closing bracket, check the top of the stack
+            const topElement = stack.length > 0 ? stack.pop() : '#';
+            if (matchingBrackets[char] !== topElement) {
+                return false;
+            }
+        } else {
+            // If it's an opening bracket, push to stack
+            stack.push(char);
+        }
+    }
+
+    // If stack is empty, all brackets were matched properly
+    return stack.length === 0;
 };
 
 module.exports = { isValid };
--- program2.js
+++ program2.js
@@ -3,7 +3,27 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    
+    const romanValues = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+    let total = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        // Check if current numeral is smaller than the next numeral
+        if (i < s.length - 1 && romanValues[s[i]] < romanValues[s[i + 1]]) {
+            total -= romanValues[s[i]];  // Subtract if smaller numeral is before larger one
+        } else {
+            total += romanValues[s[i]];  // Otherwise, add the numeral's value
+        }
+    }
+
+    return total;
 };
 
 
